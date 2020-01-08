6889f2e80e2f494459389b21d41900854320cee8
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-01-08 15:53:59 -0500

    complete

--- script.js
+++ script.js
@@ -57,7 +57,22 @@ class Calculator {
   }
 
   getDisplayNumber(number) {
-    return number;
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
   }
 
   updateDisplay() {
@@ -68,6 +83,8 @@ class Calculator {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
         this.previousOperand
       )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
