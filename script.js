087628b745742aa6ac52acf7009f0a1ca02198d7
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-01-08 14:49:28 -0500

    append function complete

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Calculator {
   delete() {}
 
   appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
