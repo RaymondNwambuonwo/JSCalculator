36542f65be8bbf62c0119c4d0ef70ee2dadb2b27
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-01-08 14:44:56 -0500

    buttons work and numbers show

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
   }
 
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
+    this.operation = undefined;
   }
 
   delete() {}
 
-  appendNumber(number) {}
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
 
   chooseOperation(operation) {}
 
   compute() {}
 
-  updateDisplay() {}
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
 }
 
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -31,3 +37,15 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
